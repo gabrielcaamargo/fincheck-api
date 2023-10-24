ace73efa28fff4c399d200198d7380e06c199e7b
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-10-24 13:37:00 -0300

    feat: create global auth guard that sets every route as private

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UsersService {
   getUserById(userId: string) {
+    console.log({ meUserId: userId })
     return { userId };
   }
 }
